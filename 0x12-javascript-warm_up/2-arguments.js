70b8cc5903c84998aef3fa927239d24f2e6956e3
NDAYISENGAManu <webmasterio2015@gmail.com>
2023-12-12 09:01:15 +0200

    2-arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
-#!/home/endayisenga@bktechouse.net/.nvm/versions/node/v16.19.1/bin/node
+#!/usr/bin/node
 
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
